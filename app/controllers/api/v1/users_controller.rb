559557e5ad45dc905d53ca7fa43b17880088fa8c
Nick Hall <nickhall122@gmail.com>
2017-12-20 13:12:59 -0500

    adding party transaction

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -14,15 +14,12 @@ class Api::V1::UsersController < ActionController::API
   def create
     @address = (params[:address] + ' ' + params[:city] + ' ' + params[:state] + ' ' + params[:zip])
     @apiKey =  ENV["GOOGLE_API_KEY"]
-
     @route = "https://maps.googleapis.com/maps/api/geocode/json?address=#{@address}&key=#{@apiKey}"
     response = HTTParty.get(@route)
-
     @latitude = response.parsed_response['results'][0]['geometry']['location']['lat']
     @longitude = response.parsed_response['results'][0]['geometry']['location']['lng']
     @user = User.create(username: params[:username], password: params[:password], email: params[:email],firstname: params[:firstname], lastname: params[:lastname], zip: params[:zip], address: params[:address], phone: params[:phone], city: params[:city], state: params[:state], latitude: @latitude, longitude: @longitude )
     render json: @user.to_json
-    # debugger
   end
 
   def update
